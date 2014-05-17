1b5b553c592b583d7d7884c08aacde63aa2872be
Tiago Martins Araújo <tiago.martins.araujo@gmail.com>
2014-05-17 11:48:00 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+I9dayImobiliaria::Application.config.secret_key_base = '24f0951ed8c6292ed2727bf6528e27c9f314bac8aff418a758cd8acf2cace01f3597bb0c238acea3cee90dbc9177be7990da9346737869f47ecdcee026231678'
